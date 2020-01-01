e34bdd39a44283b06cf90cfefd8caff82b28ddc5
Thommgb <thom.mzg@gmail.com>
2020-01-01 11:58:06 +0100

    text and chest

--- Game.swift
+++ Game.swift
@@ -26,3 +26,53 @@ class Character {
         self.characterType = characterType
     }
 }
+
+//Toutes les phrases d'intro et d'histoire.
+
+class Texte {
+    func story() {
+        print("Il était une fois nanana..")
+    }
+    
+    func shooseYourTeam() {
+        print("Bonjour Voyageur ! Vous devrez choisir trois des quatre classes suivantes afin de vous créer trois personnages !"
+        + "\n1. \(CharacterType.knight.rawValue)"
+        + "\n2. \(CharacterType.wizard.rawValue)"
+        + "\n3. \(CharacterType.giant.rawValue)"
+        + "\n4. \(CharacterType.thief.rawValue)")
+    }
+    
+    func listingCharacter() {
+    if let choice = readLine() {
+            switch choice {
+                case "\(CharacterType.knight.rawValue)":
+                    CharacterType.knight
+                print("Le chevalier, une classe puissante et indispensable de toute bonne équipe! Donnez lui un nom.")
+                    
+                case "\(CharacterType.wizard.rawValue)":
+                    CharacterType.wizard
+                print("Vous ne passerez pas ! Capable de grands soins et de sérieux dégats le magicien est idéal. Donnez lui un nom.")
+                
+                case "\(CharacterType.giant.rawValue)":
+                    CharacterType.giant
+                print("Très bon choix! Écrasez vos ennemis sans pitié. Donnez lui un nom.")
+                
+                case "\(CharacterType.thief.rawValue)":
+                CharacterType.thief
+                print("Le voleur est fourbe et rapide ! A vous de jouer. Donnez lui un nom.")
+                
+                default:
+                print("HOP HOP HOP, on revient par ici")
+                
+                }
+            }
+        
+        func openChest() {
+            print("Vous avez eu un coffre!")
+            if Bool.random() {
+            print("Une nouvelle arme a été découverte ! l'épée s'équipe automatiquement au chevalier. ")
+            if false {
+                    print("Pas de chance le coffre est vide !")
+                }
+            }
+        }
